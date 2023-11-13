99e03e0a9986140dd9bb569128c632ece81fbe82
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-11-13 14:21:20 +0100

    Refactor benchmark code (#2)
    * Update benchmark.py
    
    * Update requirements.txt
    
    * upgrade_lock
    
    * psutil
    
    * Update benchmark.py
    
    * Update benchmark.py
    
    * Update benchmark.py
    
    * Update benchmark.py
    
    * Update README.md
    
    ---------
    
    Co-authored-by: root <root@localhost.localdomain>

--- README.md
+++ README.md
@@ -12,23 +12,30 @@ Benchmark queries for supported databases are executed within Github Actions. <b
 **Benchmarks:**
 
 > [Github Action](https://github.com/lmangani/embedded-olap-benchmarks/actions/workflows/benchmarks.yml) Free Runner _(2x vCPU, 7GB RAM)_
+
+<!-- START-RESULTS -->
 ```
-chdb:version: avg=0.015s min=0.015s max=0.016s (3 runs)
-chdb:count: avg=0.195s min=0.099s max=0.354s (3 runs)
-chdb:groupby: avg=0.487s min=0.482s max=0.497s (3 runs)
-chdb:groupby-local: avg=0.362s min=0.359s max=0.364s (3 runs)
+Testing chdb 23.10.1.1
+chdb:version: avg=0.012s min=0.011s max=0.014s (3 runs) | Memory used: -2.47 MB
+chdb:count: avg=0.135s min=0.064s max=0.264s (3 runs) | Memory used: 3.91 MB
+chdb:groupby: avg=0.435s min=0.407s max=0.478s (3 runs) | Memory used: 25.98 MB
+chdb:groupby-local: avg=0.351s min=0.348s max=0.355s (3 runs) | Memory used: 6.99 MB
 
-duckdb:version: avg=0.000s min=0.000s max=0.000s (3 runs)
-duckdb:count: avg=0.361s min=0.101s max=0.875s (3 runs)
-duckdb:groupby: avg=0.619s min=0.560s max=0.722s (3 runs)
-duckdb:groupby-local: avg=0.352s min=0.340s max=0.372s (3 runs)
+Testing duckdb 0.9.1
+duckdb:version: avg=0.001s min=0.000s max=0.001s (3 runs) | Memory used: 2.96 MB
+duckdb:count: avg=0.360s min=0.083s max=0.900s (3 runs) | Memory used: 26.02 MB
+duckdb:groupby: avg=0.697s min=0.685s max=0.715s (3 runs) | Memory used: 25.86 MB
+duckdb:groupby-local: avg=0.527s min=0.524s max=0.529s (3 runs) | Memory used: 0.62 MB
 
-glaredb:version: avg=0.001s min=0.000s max=0.002s (3 runs)
-glaredb:count: avg=0.176s min=0.113s max=0.300s (3 runs)
-glaredb:groupby: avg=0.905s min=0.866s max=0.949s (3 runs)
-glaredb:groupby-local: avg=0.666s min=0.623s max=0.689s (3 runs)
+Testing glaredb: latest
+glaredb:version: avg=0.001s min=0.000s max=0.001s (3 runs) | Memory used: 11.38 MB
+glaredb:count: avg=0.157s min=0.071s max=0.307s (3 runs) | Memory used: 9.00 MB
+glaredb:groupby: avg=0.489s min=0.482s max=0.496s (3 runs) | Memory used: 200.90 MB
+glaredb:groupby-local: avg=0.363s min=0.353s max=0.378s (3 runs) | Memory used: 156.86 MB
 
 ```
+<!-- END-RESULTS -->
+
 
 ## [Example Action Report](https://github.com/lmangani/embedded-olap-benchmarks/actions/workflows/benchmarks.yml)
 ![image](https://github.com/lmangani/embedded-db-benchmarks/assets/1423657/e5961b6e-9775-4f18-adde-edbb52b672f2)
--- benchmark.py
+++ benchmark.py
@@ -1,19 +1,16 @@
-from contextlib import contextmanager
 import os
 import sys
+import psutil
 from datetime import datetime
+from contextlib import contextmanager
+
 import duckdb
 import chdb
 from chdb import session as chs
 import glaredb
 
-DBNAME = os.environ['DBNAME'] or '*'
-
-# Number of times to benchmark each query
-ITERATIONS = os.environ['ITERATIONS'] or 3
-ITERATIONS = int(ITERATIONS)
-
-# Names of queries to load from the "./queries/" folder
+DBNAME = os.getenv('DBNAME', '*')
+ITERATIONS = int(os.getenv('ITERATIONS', 3))
 BENCHMARKS = ["version", "count", "groupby", "groupby-local"]
 
 @contextmanager
@@ -26,55 +23,57 @@ def suppress_stdout():
         finally:
             sys.stdout = old_stdout
 
-def benchmark_db(db, execute_fn):
+def get_memory_usage():
+    """ Returns the current memory usage of the Python process. """
+    process = psutil.Process(os.getpid())
+    return process.memory_info().rss / (1024 * 1024)  # Convert bytes to megabytes
+
+def load_query(db: str, name: str) -> str:
+    """ Load SQL query from file. """
+    try:
+        with open(f"queries/{name}.{db}.sql") as f:
+            return f.read()
+    except FileNotFoundError:
+        print(f"Query file for {name} not found.")
+        sys.exit(1)
+
+def benchmark_db(db: str, execute_fn):
     """ Benchmarks all queries against one datastore """
     for name in BENCHMARKS:
-        # Load query
-        with open(f"queries/{name}.{db}.sql") as f:
-            query = f.read()
-        
-        # Run benchmark and track query durations
+        query = load_query(db, name)
         deltas = []
+        mem_usage_before = get_memory_usage()
         for _ in range(ITERATIONS):
             start = datetime.now()
             with suppress_stdout():
-                results = execute_fn(query)
+                try:
+                    results = execute_fn(query)
+                except Exception as e:
+                    print(f"Error executing query on {db}: {e}")
+                    continue
             end = datetime.now()
             deltas.append((end - start).total_seconds())
+        mem_usage_after = get_memory_usage()
 
-        # Print result
-        avg = sum(deltas) / len(deltas)
-        print("{}:{}: avg={:.3f}s min={:.3f}s max={:.3f}s ({} runs)".format(
-            db,
-            name,
-            avg,
-            min(deltas),
-            max(deltas),
-            ITERATIONS,
-        ))
+        if deltas:
+            avg = sum(deltas) / len(deltas)
+            mem_used = mem_usage_after - mem_usage_before
+            print(f"{db}:{name}: avg={avg:.3f}s min={min(deltas):.3f}s max={max(deltas):.3f}s ({ITERATIONS} runs) | Memory used: {mem_used:.2f} MB")
 
 def main():
     match DBNAME:
         case "chdb":
-            print("Testing chdb")
+            print("Testing chdb " + str(chdb.engine_version))
             chdbs = chs.Session()
             benchmark_db("chdb", lambda query: chdb.query(query))
         case "duckdb":
-            print("Testing duckdb")
+            print("Testing duckdb " + str(duckdb.__version__))
             ddb = duckdb.connect()
             benchmark_db("duckdb", lambda query: ddb.execute(query))
         case "glaredb":
             print("Testing glaredb")
             gdb = glaredb.connect()
             benchmark_db("glaredb", lambda query: gdb.sql(query).show())
-        case _:
-            print("Testing all databases.")
-            chdbs = chs.Session()
-            benchmark_db("chdb", lambda query: chdb.query(query))
-            ddb = duckdb.connect()
-            benchmark_db("duckdb", lambda query: ddb.execute(query))
-            gdb = glaredb.connect()
-            benchmark_db("glaredb", lambda query: gdb.sql(query).show())
-
+        
 if __name__ == "__main__":
     main()
