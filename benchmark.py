45c37a77407e408b7affbce4bc742e0c62780a6d
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-11-13 11:07:30 +0100

    Suppress Output

--- benchmark.py
+++ benchmark.py
@@ -1,4 +1,6 @@
+from contextlib import contextmanager
 import os
+import sys
 from datetime import datetime
 import duckdb
 import chdb
@@ -14,6 +16,16 @@ ITERATIONS = int(ITERATIONS)
 # Names of queries to load from the "./queries/" folder
 BENCHMARKS = ["version", "count", "groupby", "groupby-local"]
 
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+
 def benchmark_db(db, execute_fn):
     """ Benchmarks all queries against one datastore """
     for name in BENCHMARKS:
@@ -25,7 +37,8 @@ def benchmark_db(db, execute_fn):
         deltas = []
         for _ in range(ITERATIONS):
             start = datetime.now()
-            execute_fn(query)
+            with suppress_stdout():
+                results = execute_fn(query)
             end = datetime.now()
             deltas.append((end - start).total_seconds())
 
