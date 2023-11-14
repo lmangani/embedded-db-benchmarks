b8bae16f2055f6f601c5687ee257695d6dab1dcd
root <root@localhost.localdomain>
2023-11-14 03:26:08 +0100

    fix tests

--- queries/groupby.databend.sql
+++ queries/groupby.databend.sql
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-SELECT arrow_typeof(tpep_pickup_datetime::date) as day, 
-       "PULocationID" as location, 
-=======
 SELECT TO_DATE(tpep_pickup_datetime::date) as day, 
        PULocationID as location, 
->>>>>>> 669ad86a96dfcc208bb27d08e44dfe51ee4c7fb9
        count(*) as trips, 
        sum(fare_amount) + sum(mta_tax) + sum(tolls_amount) + sum(tip_amount) as revenue 
 FROM 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet' 
