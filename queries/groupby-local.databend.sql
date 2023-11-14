474dc3d87a640c2ddda39dc6befba84f10350c23
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-11-14 02:40:28 +0100

    Update groupby-local.databend.sql

--- queries/groupby-local.databend.sql
+++ queries/groupby-local.databend.sql
@@ -1,8 +1,8 @@
-SELECT arrow_typeof(tpep_pickup_datetime::date) as day, 
-       "PULocationID" as location, 
+SELECT TO_DATE(tpep_pickup_datetime::date) as day, 
+       PULocationID as location, 
        count(*) as trips, 
        sum(fare_amount) + sum(mta_tax) + sum(tolls_amount) + sum(tip_amount) as revenue 
-FROM 'yellow_tripdata_2023-01.parquet' 
+FROM 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet' 
 WHERE trip_distance > 5 
 GROUP BY tpep_pickup_datetime, location
 ORDER BY day
